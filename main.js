d887fa5a9ffff66e9cb7f6f11a6cfaa10dd34fa4
descartez <rvanstong@gmail.com>
2015-04-06 22:53:08 -0700

    adds in working jquery, begins building game engine

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-$(document).ready(
-  $('#startup').on('click', function() {
-    $('body').css('background-color', 'red');
-    console.log('wat');
-  })
+$(document).ready( function() {
+  var gameBoard = [];
+  var gameStart = false
+  $(document).on("keyup", function() {
+    if (gameStart === false){
+      fadeTitle();
+      gameStart = true
+    }else{
+      console.log('Everything falls apart.')
+    }
+  });
+
+  var fadeTitle = function() {
+    $('body').css('background-color', '#274D00');
+    $('#title').fadeOut();
+  }
+
+}
 )
